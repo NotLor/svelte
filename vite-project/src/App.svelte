dacf7d5d8e21cd2cfb18db54f3eff94d7e23efaf
Lorenzo De Angelis <125358763+NotLor@users.noreply.github.com>
2023-12-20 09:38:54 +0000

    z

---
+++ vite-project/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import AddForm from "./lib/AddForm.svelte";
+  import ListaTodo from "./lib/ListaTodo.svelte";
+  import TodoCounter from "./lib/TodoCounter.svelte";
+  import { todos } from "./lib/store";
+</script>
+
+<main>
+  <div class="p-3">
+  <h1>TODO List App</h1>
+  <TodoCounter />
+  <AddForm />
+  <ListaTodo todoList={$todos} /></div>
+</main>
+
+<style>
+</style>
---
+++ vite-project/src/lib/AddForm.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { todos } from "./store";
+  let nuovoTodo;
+  function AddTodo() {
+    todos.update((old) => {
+      // prevent default form
+      event.preventDefault();
+      old.push({ title: nuovoTodo, done: false });
+      // reset input
+      nuovoTodo = "";
+      return old;
+    });
+  }
+</script>
+
+<form class="flex">
+  <input
+    type="text"
+    placeholder="Scrivi qui"
+    class="input input-bordered w-full max-w-xs mr-1"
+    bind:value={nuovoTodo}
+  />
+  <button on:click={AddTodo} class="btn btn-primary">Aggiungi</button>
+</form>
